79ebd8ea5e4ef1e6e712818c05ca2bd2bbd9b9a3
philbjern <filipbiernat@gmail.com>
2025-09-18 09:49:59 +0200

    initial commit

---
+++ src/main/java/com/archloner/homeiotserver/HomeIoTServerApplication.java
@@ -0,0 +1,13 @@
+package com.archloner.homeiotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeIoTServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeIoTServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archloner/homeiotserver/HomeIoTServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archloner.homeiotserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeIoTServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
